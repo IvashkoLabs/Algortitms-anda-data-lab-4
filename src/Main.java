11b091ea576bd8b87b13b7fe3b458ad5d5a68751
HoatzinDev <9240847@stud.kai.edu.ua>
2026-04-05 20:04:33 +0300

    level 3

--- src/Main.java
+++ src/Main.java
@@ -2,87 +2,163 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("creating students\n");
         Random rand = new Random();
-        Student[] allStudents = new Student[10];
+        //Student[] allStudents = new Student[10];
+        List<Student> allStudents= new ArrayList<>();
+        for (int i = 0; i < 10; i++)
+        {
+            Student s = new Student();
 
-        for (int i = 0; i < 10; i++) {
-            allStudents[i] = new Student();
-            allStudents[i].surname = Surnames.values()[rand.nextInt(Surnames.values().length)];
-            allStudents[i].isMale = rand.nextBoolean();
+            s.surname = Surnames.values()[rand.nextInt(Surnames.values().length)];
+            s.isMale = rand.nextBoolean();
 
-            if (allStudents[i].isMale) {
-                allStudents[i].name = MaleNames.values()[rand.nextInt(MaleNames.values().length)].name();
-            } else {
-                allStudents[i].name = FemaleNames.values()[rand.nextInt(FemaleNames.values().length)].name();
-            }
+            if (s.isMale)
+                s.name = MaleNames.values()[rand.nextInt(MaleNames.values().length)].name();
+            else
+                s.name = FemaleNames.values()[rand.nextInt(FemaleNames.values().length)].name();
 
-            allStudents[i].group = rand.nextInt(7) + 1;
+            s.group = rand.nextInt(7) + 1;
 
-            allStudents[i].abcOrder = GetAbcOrder(allStudents[i].name, allStudents[i].surname.name());
+            s.abcOrder = GetAbcOrder(s.name, s.surname.name());
+
+            allStudents.add(s);
         }
-
+        List<Student> allStudents2 = new ArrayList<>(allStudents);
         System.out.println("random unsorted students");
         printStudents(allStudents);
 
         System.out.println("\n\nusing Selection Sort");
         SelectionSort(allStudents);
-
         System.out.println("\n\nsorted students");
         printStudents(allStudents);
-    }
-    public static void SelectionSort(Student[] stud) {
-        int size = stud.length;
 
-        for (int i = 0; i < size - 1; i++) {
-            Student min = stud[i];
+        System.out.println("\n\nusing Quick Sort");
+        Counter counter = new Counter();
+        QuickSort(allStudents2, 0, allStudents2.size() - 1,counter);
+
+        System.out.println("\nTOTAL QuickSort iterations: " + counter.value);
+        System.out.println("\n\nsorted students");
+        printStudents(allStudents2);
+    }
+    public static void SelectionSort(List<Student> stud)
+    {
+        int size = stud.size();
+        int iterations = 0;
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            Student min = stud.get(i);
             int minIndex = i;
 
-            for (int j = i + 1; j < size; j++) {
-                if (stud[j].abcOrder < min.abcOrder) {
-                    min = stud[j];
+            for (int j = i + 1; j < size; j++)
+            {
+                iterations++;
+
+                if (stud.get(j).abcOrder < min.abcOrder)
+                {
+                    min = stud.get(j);
                     minIndex = j;
                 }
             }
 
             System.out.println("\niteration " + (i + 1));
-            System.out.printf("looking for smallest from %d \n", i);
+            System.out.println("total comparisons so far: " + iterations);
+            System.out.printf("looking for smallest from index %d\n", i);
 
-            if (minIndex != i) {
-                System.out.printf("Found: [%s %s]. Switch with [%s %s].\n",
-                        min.surname.name(), min.name,
-                        stud[i].surname.name(), stud[i].name);
+            if (minIndex != i)
+            {
+                System.out.printf("Found: [%s %s] → swap with [%s %s]\n",
+                        min.name, min.surname.name(),
+                        stud.get(i).name, stud.get(i).surname.name());
 
-                // Сам процес обміну (Swap)
-                stud[minIndex] = stud[i];
-                stud[i] = min;
-            } else {
-                System.out.printf("[%s %s] is already the smallest so skip.\n",
-                        stud[i].surname.name(), stud[i].name);
+                Student temp = stud.get(i);
+                stud.set(i, min);
+                stud.set(minIndex, temp);
+            }
+            else
+            {
+                System.out.printf("[%s %s] already smallest so skip\n",
+                        stud.get(i).name, stud.get(i).surname.name());
             }
 
-            System.out.print("current list: [ ");
-            for (int k = 0; k < size; k++) {
-                if (k <= i) {
-                    System.out.print("*" +stud[k].name+ " "+stud[k].surname.name() + "* ");
-                } else {
-                    System.out.print(stud[k].name+ " "+stud[k].surname.name() + " ");
-                }
-            }
-            System.out.println("]");
+            printCurrentState(stud, i);
         }
+
+        System.out.println("\nTOTAL iterations: " + iterations);
     }
-public static double GetAbcOrder(String name, String surname) {
+    public static void printCurrentState(List<Student> stud, int sortedBorder)
+    {
+        System.out.print("current list: [ ");
+
+        for (int k = 0; k < stud.size(); k++)
+        {
+            Student s = stud.get(k);
+
+            if (k <= sortedBorder)
+            {
+                System.out.print("*" + s.name + " " + s.surname.name() + "* ");
+            }
+            else
+            {
+                System.out.print(s.name + " " + s.surname.name() + " ");
+            }
+        }
+
+        System.out.println("]");
+    }
+    public static void QuickSort(List<Student> stud, int low, int high, Counter counter)
+    {
+        if (low >= high) return;
+
+        int i = low;
+        int j = high;
+        double anchor = stud.get((low + high) / 2).abcOrder;
+
+        System.out.println("\nNew iteration:"+ counter.value+" [" + low + ", " + high + "] anchor =" + anchor );
+
+        while (i <= j)
+        {
+            while (stud.get(i).abcOrder < anchor ) i++;
+            while (stud.get(j).abcOrder > anchor ) j--;
+
+            if (i <= j)
+            {
+                System.out.printf("swapped %s %s with %s %s\n",
+                        stud.get(i).name,stud.get(i).surname, stud.get(j).name,stud.get(j).surname );
+
+                Student temp = stud.get(i);
+                stud.set(i, stud.get(j));
+                stud.set(j, temp);
+
+                i++;
+                j--;
+            }
+        }
+
+        printCurrentState(stud, -1);
+        counter.value++;
+        QuickSort(stud, low, j,counter);
+        QuickSort(stud, i, high, counter);
+    }
+    public static class Counter {
+        int value = 0;
+    }
+    public static double GetAbcOrder(String name, String surname)
+    {
         double result = 0.0;
 
         String fullName = (name + surname).toUpperCase();
 
         double divisor = 27.0;
 
-        for (int i = 0; i < fullName.length(); i++) {
+        for (int i = 0; i < fullName.length(); i++)
+        {
             char c = fullName.charAt(i);
-            if (c >= 'A' && c <= 'Z') {
+            if (c >= 'A' && c <= 'Z')
+            {
                 double value = (c - 'A' + 1); // A=1, B=2, ..., Z=26
                 result += value / divisor;
                 divisor *= 27.0;              // every other means less
@@ -92,21 +168,24 @@ public static double GetAbcOrder(String name, String surname) {
     }
 
 
-    public static void printStudents(Student[] students) {
-        for (Student s : students) {
+    public static void printStudents(List<Student> students)
+    {
+        for (Student s : students)
+        {
             System.out.printf("%-10s %-10s | is male: %-5b | group: %d | abc order: %.8f\n",
                     s.name,s.surname.name(), s.isMale, s.group, s.abcOrder);
         }
     }
 }
-class Student// implements Comparable<Student> {
-    {
+class Student// implements Comparable<Student>
+{
+
     Surnames surname;
     String name;
     //int created;
     int group;
     boolean isMale;
-        double abcOrder;
+    double abcOrder;
 }
 enum Surnames
 {
